81973ec2626abbdf5b7bb763cf2d6d5593c69709
Raianedantas <112406065+Raianedantas@users.noreply.github.com>
2023-11-18 14:42:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+
+   //função para Altura do gancho 
+function evalInput(strInput) {
+    // Valor fixo
+    var valorFixo = "alturadogancho=";
+
+    // Adiciona o valor fixo ao valor inserido pelo usuário
+    var resultado = valorFixo + strInput;
+
+    // Executa o comando com o resultado
+    ggbApplet.evalCommand(resultado.toString());
+
+    // Limpa o campo de entrada
+    document.getElementById('campo').value = '';
+
+    // Retorna falso para evitar o envio do formulário
+    return false;
+    }
+    
+    //função para o Altura do quadril 
+		function evalInput1(strInput) {
+    // Valor fixo
+    var valorFixo = "alturadoquadril=";
+
+    // Adiciona o valor fixo ao valor inserido pelo usuário
+    var resultado = valorFixo + strInput;
+
+    // Executa o comando com o resultado
+    ggbApplet.evalCommand(resultado.toString());
+
+    // Limpa o campo de entrada
+    document.getElementById('campo').value = '';
+
+    // Retorna falso para evitar o envio do formulário
+    return false;
+    }
+
+//função para a altura do cintura 
+		function evalInput2(strInput) {
+    // Valor fixo
+    var valorFixo = "cintura=";
+
+    // Adiciona o valor fixo ao valor inserido pelo usuário
+    var resultado = valorFixo + strInput;
+
+    // Executa o comando com o resultado
+    ggbApplet.evalCommand(resultado.toString());
+
+    // Limpa o campo de entrada
+    document.getElementById('campo').value = '';
+
+    // Retorna falso para evitar o envio do formulário
+    return false;
+    }
+    
+    //função para o comprimento do joelho
+		function evalInput3(strInput) {
+    // Valor fixo
+    var valorFixo = "comprimentodojoelho=";
+
+    // Adiciona o valor fixo ao valor inserido pelo usuário
+    var resultado = valorFixo + strInput;
+
+    // Executa o comando com o resultado
+    ggbApplet.evalCommand(resultado.toString());
+
+    // Limpa o campo de entrada
+    document.getElementById('campo').value = '';
+
+    // Retorna falso para evitar o envio do formulário
+    return false;
+    }
+
+ //função para o comprimento total 
+		function evalInput4(strInput) {
+    // Valor fixo
+    var valorFixo = "comprimentototal=";
+
+    // Adiciona o valor fixo ao valor inserido pelo usuário
+    var resultado = valorFixo + strInput;
+
+    // Executa o comando com o resultado
+    ggbApplet.evalCommand(resultado.toString());
+
+    // Limpa o campo de entrada
+    document.getElementById('campo').value = '';
+
+    // Retorna falso para evitar o envio do formulário
+    return false;
+    }
+
+ //função para o largura da barra
+		function evalInput5(strInput) {
+    // Valor fixo
+    var valorFixo = "larguradabarra=";
+
+    // Adiciona o valor fixo ao valor inserido pelo usuário
+    var resultado = valorFixo + strInput;
+
+    // Executa o comando com o resultado
+    ggbApplet.evalCommand(resultado.toString());
+
+    // Limpa o campo de entrada
+    document.getElementById('campo').value = '';
+
+    // Retorna falso para evitar o envio do formulário
+    return false;
+    }
+
+
+
+//função para o separação do quadril
+		function evalInput6(strInput) {
+    // Valor fixo
+    var valorFixo = "quadril=";
+
+    // Adiciona o valor fixo ao valor inserido pelo usuário
+    var resultado = valorFixo + strInput;
+
+    // Executa o comando com o resultado
+    ggbApplet.evalCommand(resultado.toString());
+
+    // Limpa o campo de entrada
+    document.getElementById('campo').value = '';
+
+    // Retorna falso para evitar o envio do formulário
+    return false;
+    }
+
+
+
